db3d8a9e000018dd73655b766f568b087220ce90
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-12 13:49:02 -0300

    stylizing items

--- restaurant/src/components/Item.vue
+++ restaurant/src/components/Item.vue
@@ -25,5 +25,49 @@ export default {
 </script>
 
 <style scoped lang="less">
-
+.item{
+    width: 216px;
+    height: fit-content;
+    border-radius: 10px;
+    background: white;
+    position: relative;
+    margin: 20px;
+    &--tag{
+        position: absolute;
+        background: @pink;
+        border-radius: 8px;
+        color: #fff;
+        top: 15px;
+        right: 15px;
+        font-size: 12px;
+        font-weight: 500;
+        padding: 3px 8px;
+    }
+    &--img{
+        display: block;
+        margin: 30px auto;
+    }
+    img{
+        width: 140px;
+        height: auto;
+    }
+    &--name{
+        font-weight: 600;
+        font-size: 18px;
+        margin: 10px;
+    }
+    &--description{
+        color: @dark-grey;
+        font-weight: 300;
+        font-size: 12px;
+        margin: 0 10px;
+    }
+    &--price{
+        font-weight: 600;
+        font-size: 18px;
+        color: @yellow;
+        margin: 0;
+        padding: 10px;
+    }
+}
 </style>
