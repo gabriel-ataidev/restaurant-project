bf4d32bb07514cbf4471dff61fa377b02b3f297f
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-12 12:44:55 -0300

    creating ItemsList

---
+++ restaurant/src/components/ItemsList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="items-list">
+    {{ itemsList }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ItemsList",
+  data() {
+      return {
+          itemsList: []
+      }
+  },
+  created() {
+    axios.get("http://localhost:3000/burguers").then((response) => {
+      this.itemsList = response.data;
+    });
+  },
+};
+</script>
+
+<style></style>
