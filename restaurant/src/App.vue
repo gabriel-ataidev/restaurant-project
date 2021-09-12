a3584fc0f5f9502f55558312e6d6de727ecc30c0
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-11 22:17:00 -0300

    vue-router installed

--- restaurant/src/App.vue
+++ restaurant/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-    <CategoryMenu/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import CategoryMenu from "./components/CategoryMenu.vue";
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    CategoryMenu,
-  },
-};
-</script>
+#nav {
+  padding: 30px;
 
-<style>
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
