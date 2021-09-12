0572cae1a7a8e78228000e98c662cd28be052d8a
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-11 22:23:48 -0300

    vue router all set

--- restaurant/src/views/Home.vue
+++ restaurant/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CategoryMenu />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CategoryMenu from '@/components/CategoryMenu.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CategoryMenu
   }
 }
 </script>
