31a893211344023c3396bf89c29c0c642c0e51fe
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-14 19:14:54 -0300

    disabling quantity when it hits 0

--- restaurant/src/components/CartItem.vue
+++ restaurant/src/components/CartItem.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="item">
     <div class="item--quantity">
-      <span class="buttons" @click="decreaseQuantity(item.id)">-</span>
+      <button class="buttons" @click="decreaseQuantity(item.id)" :disabled="item.quantity == 0">-</button>
       <span class="number">{{ item.quantity }}</span>
-      <span class="buttons" @click="increaseQuantity(item.id)">+</span>
+      <button class="buttons" @click="increaseQuantity(item.id)">+</button>
     </div>
     <div class="item--img-container">
       <img class="item--img" :src="imagePath" />
@@ -60,6 +60,14 @@ export default {
     }
     .buttons{
       cursor: pointer;
+      font-size: 20px;
+      padding: 0;
+      background: none;
+      border: 0;
+      outline: none;
+      :focus {
+        outline: 0;
+      }
     }
   }
   &--img-container {
