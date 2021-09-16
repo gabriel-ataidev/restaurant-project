ea0451d2ba867b0ad4a4393041ebe4b69c742e69
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-16 10:50:31 -0300

    creating quantity

---
+++ restaurant/src/components/Quantity.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="item--quantity">
+    <button
+      class="buttons"
+      @click="decreaseQuantity(item.id)"
+      :disabled="item.quantity == 0"
+    >
+      -
+    </button>
+    <span class="number">{{ item.quantity }}</span>
+    <button class="buttons" @click="increaseQuantity(item.id)">+</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  props: {
+    item: {},
+  },
+  methods: {
+    ...mapActions(["increaseQuantity", "decreaseQuantity"]),
+  },
+};
+</script>
+
+<style></style>
