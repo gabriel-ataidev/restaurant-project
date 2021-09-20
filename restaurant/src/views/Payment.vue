78b44fa84be6b28243bfddb03459f7ab0da42c6f
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2021-09-20 13:47:55 -0300

    setting up payment vue

--- restaurant/src/views/Payment.vue
+++ restaurant/src/views/Payment.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-      pagamento
+  <div class="payment">
+    <div class="payment--content">
+      <router-link to="/" class="payment--go-back"> ←️ Voltar</router-link>
+    </div>
+    <Cart v-if="isDesktop()" />
   </div>
 </template>
 
 <script>
-export default {
+import Cart from "@/components/Cart.vue";
+import Mixin from "@/mixins/mixins";
 
-}
+export default {
+  components: {
+    Cart,
+  },
+  mixins: [Mixin],
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.payment {
+  display: flex;
+  &--content {
+    flex: 1;
+    padding: 50px;
+    .payment--go-back {
+      font-weight: 600;
+      font-size: 20px;
+      color: @yellow;
+      text-decoration: none;
+    }
+  }
+}
 </style>
